928268f4b47b21a280ed33fc1c20966cf315a537
Eric <egregory04@gmail.com>
2022-04-11 20:52:41 -0400

    Initial commit

---
+++ todo-app/index.js
@@ -0,0 +1,35 @@
+// Dependencies
+
+const mysql = require("mysql2");
+const express = require("express");
+
+// MySQL config - Be careful of passwords!
+
+const db = mysql.createConnection({
+    host: "todo-mysql",
+    user: "root",
+    password: "octoberfest",
+});
+
+db.connect((err) => {
+    if (err) {
+      console.log("Error occurred", err);
+    } else {
+      dbStatus = "Connected to MySQL Server";
+      console.log(`${dbStatus}`);
+    }
+});
+
+// Express config
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.get("/api", (req, res) => {
+  res.json({ message: `${dbStatus}` });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+});
+
